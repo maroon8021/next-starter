1f1da463902c5b49cd1169fbff8241abd3b5263d
Tatsuhiko <okihiutat.iutim@gmail.com>
2020-05-19 17:41:55 +0900

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,40 @@
 import { NextComponentType } from "next"
 import { Global, css } from "@emotion/core"
+import Head from "next/head"
 import reset from "../styles/reset"
 
+const font = css`
+  html,
+  body {
+    font-family: Ubuntu, "Noto Sans JP,Noto Sans Japanese,Noto Sans,sans-serif";
+  }
+`
 
 type App = {
-  Component : NextComponentType,
-  pageProps : any
+  Component: NextComponentType
+  pageProps: any
 }
 
-function MyApp({ Component, pageProps }:App) {
-  return (<>
-  <Global
-      styles={css`
-        ${reset}
-      `}
-    />
-    <Component {...pageProps} />
-  </>
-  
+function MyApp({ Component, pageProps }: App) {
+  return (
+    <>
+      <Global
+        styles={css`
+          ${reset}
+        `}
+      />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Noto+Sans+JP"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
 
